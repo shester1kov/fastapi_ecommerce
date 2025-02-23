6e9f32ec112742f22dfcc08b4895f339ffbe2517
Dima <shesterikov02@mail.ru>
2025-02-23 16:48:44 +0300

    update Review model

--- app/models/review.py
+++ app/models/review.py
@@ -1,5 +1,6 @@
 from app.backend.db import Base
 from sqlalchemy import Column, Integer, String, Date, Boolean, ForeignKey
+from datetime import datetime, timezone
 
 
 class Review(Base):
@@ -10,5 +11,5 @@ class Review(Base):
     product_id = Column(Integer, ForeignKey('products.id'))
     rating_id = Column(Integer, ForeignKey('ratings.id'))
     comment = Column(String)
-    comment_date = Column(Date)
+    comment_date = Column(Date, default=datetime.now(timezone.utc))
     is_active = Column(Boolean, default=True)
