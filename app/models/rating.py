fa2b8e3554361440762cc0ed8eeb023b1cd2211b
Dima <shesterikov02@mail.ru>
2025-02-23 19:27:12 +0300

    add relationship in Review and Rating models

--- app/models/rating.py
+++ app/models/rating.py
@@ -1,5 +1,6 @@
 from app.backend.db import Base
 from sqlalchemy import Column, Integer, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class Rating(Base):
@@ -10,3 +11,5 @@ class Rating(Base):
     user_id = Column(Integer, ForeignKey('users.id'))
     product_id = Column(Integer, ForeignKey('products.id'))
     is_active = Column(Boolean, default=True)
+
+    review = relationship('Review', back_populates='rating', uselist=False)
--- app/models/review.py
+++ app/models/review.py
@@ -1,6 +1,7 @@
 from app.backend.db import Base
 from sqlalchemy import Column, Integer, String, Date, Boolean, ForeignKey
 from datetime import datetime, timezone
+from sqlalchemy.orm import relationship
 
 
 class Review(Base):
@@ -9,7 +10,9 @@ class Review(Base):
     id = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, ForeignKey('users.id'))
     product_id = Column(Integer, ForeignKey('products.id'))
-    rating_id = Column(Integer, ForeignKey('ratings.id'))
+    rating_id = Column(Integer, ForeignKey('ratings.id'), unique=True)
     comment = Column(String)
     comment_date = Column(Date, default=datetime.now(timezone.utc))
     is_active = Column(Boolean, default=True)
+
+    rating = relationship('Rating', back_populates='review', uselist=False, single_parent=True)
