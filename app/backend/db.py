94341aabb7302b2f153dedfab3d7d3ead833e736
Dima <shesterikov02@mail.ru>
2025-06-04 23:02:38 +0300

    add docker

--- app/backend/db.py
+++ app/backend/db.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, Asyn
 from sqlalchemy.orm import DeclarativeBase
 
 
-engine = create_async_engine('postgresql+asyncpg://ecommerce:67@localhost:5432/ecommerce', echo=True)
+engine = create_async_engine('postgresql+asyncpg://postgres_user:postgres_password@db:5432/postgres_database', echo=True)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
 
--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from loguru import logger
+from uuid import uuid4
 from app.routers import category
 from app.routers import products
 from app.routers import auth
@@ -6,6 +9,24 @@ from app.routers import permission
 from app.routers import review
 
 app = FastAPI()
+
+logger.add('info.log', format='Log: [{extra[log_id]}:{time} - {level} - {message}]', level="INFO", enqueue=True)
+
+
+@app.middleware('http')
+async def log_middleware(request: Request, call_next):
+    log_id = str(uuid4())
+    with logger.contextualize(log_id=log_id):
+        try:
+            response = await call_next(request)
+            if response.status_code in range(401, 405):
+                logger.warning(f'Request to {request.url.path} failed')
+            else:
+                logger.info('Successfully accessed ' + request.url.path)
+        except Exception as ex:
+            logger.error(f'Request to {request.url.path} failed: {ex}')
+            response = JSONResponse(content={'success': False}, status_code=500)
+        return response
 
 
 @app.get("/")
@@ -17,4 +38,3 @@ app.include_router(products.router)
 app.include_router(auth.router)
 app.include_router(permission.router)
 app.include_router(review.router)
-
